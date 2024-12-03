3ffb210d5a95c3a6002bae7486e0f7f8336c6c4d
Yusufcatursaputro <161544056+Yusufcatursaputro@users.noreply.github.com>
2024-12-03 15:48:18 +0700

    UAS 2.0

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -7,16 +7,16 @@
 <div class="alert alert-success">{{Session::get('pesan')}}</div>
 @endif
 @if(Auth::check() && Auth::user()->role == 'admin')
-<a href="{{ route('create') }}" class="btn btn-primary float-end ms-3">Tambah Buku</a>
+<div href="{{ route('create') }}" class="btn btn-primary float-end ms-3">Tambah Buku</div>
 @endif
 <form action="{{ route('search') }}" method="get">@csrf
     <input type="text" name="kata" class="form-control" placeholder="Cari ..." style="width: 30%; display: inline; float: right;">
 </form>
-<table class="table table-stripped">
+<table class="table mt-5">
     <thead>
-        <tr>
+        <tr class="table-secondary">
             <th>Thumbnail</th>
-            <th class="w-10">Judul Buku</th>
+            <th>Judul Buku</th>
             <th>Penulis</th>
             <th>Harga</th>
             <th>Diskon</th>
@@ -27,7 +27,7 @@
             @endif
         </tr>
     </thead>
-    <tbody>
+    <tbody class="table-light">
         @foreach($data_buku as $buku)
         <tr>
             <td>
@@ -45,8 +45,11 @@
             <td class="text-danger text-decoration-line-through">
                 {{ "Rp. ".number_format($buku->harga, 2, ',', '.') }}
             </td>
-            <td class="badge text-bg-success text-wrap my-2">
+            <td>
+                <a class="badge text-bg-success text-wrap my-2">
                 {{ $buku->diskon ? $buku->diskon . '%' : '0%' }}
+                </a>
+                
             </td>
             <td class="text-success">
                 Rp. {{ number_format($buku->hargaSetelahDiskon(), 2, ',', '.') }}
@@ -77,11 +80,70 @@
 <div>{{ $data_buku->links('pagination::bootstrap-5') }}</div>
 
 <h2>Editorial Picks</h2>
-<ul>
-    @foreach($editorialPicks as $buku)
-    <li>{{ $buku->judul }}</li>
-    @endforeach
-</ul>
+<table class="table">
+    <thead>
+        <tr class="table-secondary">
+            <th>Thumbnail</th>
+            <th class="w-10">Judul Buku</th>
+            <th>Penulis</th>
+            <th>Harga</th>
+            <th>Diskon</th>
+            <th>Harga Baru</th>
+            <th>Tanggal Terbit</th>
+            @if(Auth::check() && Auth::user()->role == 'admin')
+            <th>Aksi</th>
+            @endif
+        </tr>
+    </thead>
+    <tbody class="table-info">
+        @foreach($editorialPicks as $buku)
+        <tr>
+            <td>
+                @if ( $buku->filepath )
+                <div class="relative">
+                    <img
+                        class="h-full w-full rounded-full object-cover object-center"
+                        src="{{ asset($buku->filepath) }}"
+                        alt="" />
+                </div>
+                @endif
+            </td>
+            <td>{{ $buku->judul }}</td>
+            <td>{{ $buku->penulis }}</td>
+            <td class="text-danger text-decoration-line-through">
+                {{ "Rp. ".number_format($buku->harga, 2, ',', '.') }}
+            </td>
+            <td>
+                <a class="badge text-bg-success text-wrap my-2">
+                {{ $buku->diskon ? $buku->diskon . '%' : '0%' }}
+                </a>
+                
+            </td>
+            <td class="text-success">
+                Rp. {{ number_format($buku->hargaSetelahDiskon(), 2, ',', '.') }}
+            </td>
+            <td>{{ \Carbon\Carbon::parse($buku->tgl_terbit)->format('d/m/Y') }}</td>
+            <td>
+                @if(Auth::check() && Auth::user()->role == 'admin')
+                <div class="row">
+                    <div class="col-md-3 me-2">
+                        <a class="btn btn-primary" href="{{ route('edit', $buku->id) }}">Edit</a>
+                    </div>
+                    <div class="col-md-3">
+                        <form action="{{ route('destroy', $buku->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button onclick="return confirm('Yakin mau dihapus?')" type="submit"
+                                class="btn btn-danger">Hapus</button>
+                        </form>
+                    </div>
+                </div>
+                @endif
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 
 <div><strong>Jumlah Buku: {{ $jumlah_buku }}</strong></div>
 
